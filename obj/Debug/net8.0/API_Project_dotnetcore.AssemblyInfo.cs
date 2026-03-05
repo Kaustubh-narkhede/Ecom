668e33c614efd00aa4dc5f99444ca3fa36cbdb9d
KAUSTUBH <kauatubhnarkhede1111@gmail.com>
2026-03-05 22:34:48 +0530

    merge

--- obj/Debug/net8.0/API_Project_dotnetcore.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_Project_dotnetcore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Project_dotnetcore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45edb49643c9997d7e1a8ee487f297662589116e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Project_dotnetcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Project_dotnetcore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
